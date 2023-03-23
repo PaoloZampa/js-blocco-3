fb90753a045d999e4f12230c887606190ee56b70
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-23 12:56:03 +0100

    fatto

---
+++ snack-3/main.js
@@ -0,0 +1,32 @@
+/* snack 3
+Crea un array di oggetti che rappresentano degli animali.
+Ogni animale ha un nome, una famiglia e una classe.
+Es:
+[
+  { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+  { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+Crea un nuovo array con la lista dei mammiferi. */
+
+const aniamls = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+
+console.log(aniamls);
+
+const mammiferi = aniamls.filter((value) => {
+    if (value.classe == 'mammiferi') {
+        return true
+    }
+    })
+
+console.log(mammiferi);
